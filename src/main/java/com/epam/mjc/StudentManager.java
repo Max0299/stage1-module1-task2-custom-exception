b3410cd7506ac6d1451a835eb7ff437b78a0f603
Max <maxageev5@gmail.com>
2022-11-09 11:35:07 +0300

    check

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,12 +1,30 @@
 package com.epam.mjc;
 
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+    boolean isTrue = true;
+    Iterator<Long> iterator = Arrays.stream(IDs).iterator();
+    while (iterator.hasNext()){
+      if(iterator.next() == studentID){
+        isTrue = true;
+        break;
+      }
+      else {
+        isTrue = false;
+      }
+    }
+    if(!isTrue){
+      throw new StudentNotFoundException( "Could not find student with ID " + studentID);
+    }else {
+      return Student.getValueOf(studentID);
+    }
   }
 
   public static void main(String[] args) {
